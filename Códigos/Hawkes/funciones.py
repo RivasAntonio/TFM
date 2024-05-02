4627288398edc04e32dcd0856aed2cf031f695f7
RivasAntonio <antonio.r.b01@gmail.com>
2024-05-02 13:14:01 +0200

    Añadida nueva biblio, funciones, notebook figuras

---
+++ Códigos/Hawkes/funciones.py
@@ -0,0 +1,130 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def algorithm(rate, mu, n):
+    """
+    Algorithm that computes interevent times and Hawkes intensity
+
+    #Output: rate x_k, x_k
+    """                                    
+    # Paso 1
+    u1 = np.random.uniform()
+    if mu == 0:
+        F1 = np.inf
+    else:
+        F1 = -np.log(u1) / mu
+
+    # Paso 2
+    u2 = np.random.uniform()
+    if (rate - mu) == 0:
+        G2 = 0
+    else:
+        G2 = 1 + np.log(u2) / (rate - mu)
+        
+
+    # Paso 3
+    if G2 <= 0:
+        F2 = np.inf
+    else:
+        F2 = -np.log(G2)
+
+    # Paso 4
+    xk = min(F1, F2)
+
+    # Paso 5
+    rate_tk = (rate - mu) * np.exp(-xk) + n + mu
+    return rate_tk, xk 
+
+def generate_series(K, n, mu):
+    """
+    Generates temporal series for K Hawkes processes
+    
+    ##Inputs:
+    K: Number of events
+    n: Strength of the Hawkes process
+    mu: Background intensity 
+
+    ##Output:
+    times: time series the events
+    rate: time series for the intensity
+    """
+    times_between_events = [0]
+    rate = [mu]
+    for _ in range(K):
+        rate_tk, xk = algorithm(rate[-1], mu, n)
+        rate.append(rate_tk)
+        times_between_events.append(xk)
+    times = np.cumsum(times_between_events)
+    return times, rate
+
+def identify_clusters(times, delta):
+    """
+    Identifies clusters in a temporal series given a resolution parameter delta
+    
+    ## Inputs:
+    times: temporal series
+    delta: resolution parameter
+
+    ## Output:
+    clusters: list of clusters
+    """
+    clusters = []
+    current_cluster = []
+    for i in range(len(times) - 1):
+        if times[i + 1] - times[i] <= delta:
+            if not current_cluster:
+                current_cluster.append(times[i])
+            current_cluster.append(times[i + 1])
+        else:
+            if current_cluster:
+                clusters.append(current_cluster)
+                current_cluster = []
+    return clusters
+
+def generate_series_perc(K, n, mu):
+    """
+    Generates temporal series for K Hawkes processes
+    
+    ##Inputs:
+    K: Number of events
+    n: Strength of the Hawkes process
+    mu: Background intensity 
+
+    ##Output:
+    times: time series the events
+    rate: time series for the intensity
+    """
+    times_between_events = [0]
+    rate = [mu]
+    for _ in range(K):
+        rate_tk, xk = algorithm(rate[-1], mu, n)
+        rate.append(rate_tk)
+        times_between_events.append(xk)
+    times = np.cumsum(times_between_events)
+    return times_between_events, times, rate
+
+def calculate_percolation_strength(times_between_events, deltas):
+    percolation_strengths = []
+
+    for delta in deltas:
+        cluster_sizes = []
+        # Initialize the size of the current cluster
+        current_cluster_size = 1 # The first event is always a cluster
+
+        for i in range(len(times_between_events)):
+            if times_between_events[i] <= delta:
+                current_cluster_size += 1
+            else:
+                if current_cluster_size > 1: # Only consider clusters with more than one event
+                    cluster_sizes.append(current_cluster_size)
+                # Reset the size of the current cluster
+                current_cluster_size = 1 # The next event is always a cluster
+
+        # Add the size of the last cluster
+        if current_cluster_size > 1: # Only consider clusters with more than one event
+            cluster_sizes.append(current_cluster_size)
+
+        max_cluster_size = max(cluster_sizes) 
+
+        percolation_strengths.append(max_cluster_size / len(times_between_events))
+    return percolation_strengths
