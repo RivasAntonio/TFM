c5bcf3fb44019ef8decab51e39fe3d52fefd296f
RivasAntonio <antonio.r.b01@gmail.com>
2024-06-12 11:03:46 +0200

    Añadida carpeta documento

---
+++ Documento/funciones.py
@@ -0,0 +1,188 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def algorithm(rate, mu, n):
+    """
+    Algorithm that computes interevent times and Hawkes intensity
+
+    #Output: rate x_k, x_k
+    """                                    
+    # Paso 1
+    u1 = np.random.uniform()
+    if mu == 0:
+        F1 = np.inf
+    else:
+        F1 = -np.log(u1) / mu
+
+    # Paso 2
+    u2 = np.random.uniform()
+    if (rate - mu) == 0:
+        G2 = 0
+    else:
+        G2 = 1 + np.log(u2) / (rate - mu)
+        
+
+    # Paso 3
+    if G2 <= 0:
+        F2 = np.inf
+    else:
+        F2 = -np.log(G2)
+
+    # Paso 4
+    xk = min(F1, F2)
+
+    # Paso 5
+    rate_tk = (rate - mu) * np.exp(-xk) + n + mu
+    return rate_tk, xk 
+
+def generate_series(K, n, mu):
+    """
+    Generates temporal series for K Hawkes processes
+    
+    ##Inputs:
+    K: Number of events
+    n: Strength of the Hawkes process
+    mu: Background intensity 
+
+    ##Output:
+    times: time series the events
+    rate: time series for the intensity
+    """
+    times_between_events = [0]
+    rate = [mu]
+    for _ in range(K):
+        rate_tk, xk = algorithm(rate[-1], mu, n)
+        rate.append(rate_tk)
+        times_between_events.append(xk)
+    times = np.cumsum(times_between_events)
+    return times, rate
+
+def identify_clusters(times, delta):
+    """
+    Identifies clusters in a temporal series given a resolution parameter delta
+    
+    ## Inputs:
+    times: temporal series
+    delta: resolution parameter
+
+    ## Output:
+    clusters: list of clusters
+    """
+    clusters = []
+    current_cluster = []
+    for i in range(len(times) - 1):
+        if times[i + 1] - times[i] <= delta:
+            if not current_cluster:
+                current_cluster.append(times[i])
+            current_cluster.append(times[i + 1])
+        else:
+            if current_cluster:
+                clusters.append(current_cluster)
+                current_cluster = []
+    return clusters
+
+def generate_series_perc(K, n, mu):
+    """
+    Generates temporal series for K Hawkes processes
+    
+    ##Inputs:
+    K: Number of events
+    n: Strength of the Hawkes process
+    mu: Background intensity 
+
+    ##Output:
+    times_between_events: time series the interevent times
+    times: time series the events
+    rate: time series for the intensity
+    """
+    times_between_events = [0]
+    rate = [mu]
+    for _ in range(K):
+        rate_tk, xk = algorithm(rate[-1], mu, n)
+        rate.append(rate_tk)
+        times_between_events.append(xk)
+    times = np.cumsum(times_between_events)
+    return times_between_events, times, rate
+
+def calculate_percolation_strength(times_between_events, deltas):
+    percolation_strengths = []
+
+    for delta in deltas:
+        cluster_sizes = []
+        # Initialize the size of the current cluster
+        current_cluster_size = 1 # The first event is always a cluster
+
+        for i in range(len(times_between_events)):
+            if times_between_events[i] <= delta:
+                current_cluster_size += 1
+            else:
+                if current_cluster_size > 1: # Only consider clusters with more than one event
+                    cluster_sizes.append(current_cluster_size)
+                # Reset the size of the current cluster
+                current_cluster_size = 1 # The next event is always a cluster
+
+        # Add the size of the last cluster
+        if current_cluster_size > 1: # Only consider clusters with more than one event
+            cluster_sizes.append(current_cluster_size)
+
+        max_cluster_size = max(cluster_sizes) 
+
+        percolation_strengths.append(max_cluster_size / len(times_between_events))
+    return percolation_strengths
+
+def model(n_max, mu_E, mu_I, tau, n_EE, n_IE, n_EI, n_II, dt):
+    """
+    Solve the equations of the mena field model for a given number of iterations n_max
+    
+    Inputs:
+    n_max: number of iterations
+    mu_E: Poisson rate of excitatory neurons
+    mu_I: Poisson rate of inhibitory neurons
+    tau: characteristic time of the system
+    n_EE: influence of excitatory neurons on excitatory neurons
+    n_IE: influence of excitatory neurons on inhibitory neurons
+    n_EI: influence of inhibitory neurons on excitatory neurons
+    n_II: influence of inhibitory neurons on inhibitory neurons
+    dt: time step
+
+    Outputs:
+    time: time series
+    t_events_E: times of events of excitatory neurons
+    t_events_I: times of events of inhibitory neurons
+    rates_E: rates of excitatory neurons
+    rates_I: rates of inhibitory neurons
+    """
+    n_E = n_I = n = 0
+    t_events_E = [0]
+    t_events_I = [0]
+    rates_E = [mu_E]
+    rates_I = [mu_I]
+    time = [0]
+    while n <= n_max:
+        # Excitation neurons
+        l_Enew = rates_E[-1]  + dt * (mu_E- rates_E[-1])/tau
+        if np.random.uniform() < rates_E[-1]*dt:
+            l_Enew += n_EE
+            t_events_E.append(time[-1]+dt*np.random.uniform())
+            n_E += 1
+        if np.random.uniform() < rates_I[-1]*dt:
+            l_Enew -= n_IE
+            t_events_E.append(time[-1]+dt*np.random.uniform())
+            n_E += 1
+
+        # Inhibition neurons
+        l_Inew = rates_I[-1] + dt * (mu_I- rates_I[-1])/tau
+        if np.random.uniform() < rates_E[-1]*dt:
+            l_Inew += n_EI
+            t_events_I.append(time[-1]+dt*np.random.uniform())
+            n_I += 1
+        if np.random.uniform() < rates_I[-1]*dt:
+            l_Inew -= n_II
+            t_events_I.append(time[-1]+dt*np.random.uniform())
+            n_I += 1
+        rates_E.append(l_Enew)
+        rates_I.append(l_Inew)
+        time.append(time[-1]+dt)
+
+        n = n_E + n_I
+    return time, t_events_E, t_events_I, rates_E, rates_I
